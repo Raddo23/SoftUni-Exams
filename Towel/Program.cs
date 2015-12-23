6ba7d5fdcee73a4ba2fbdcb2c727a69e76126026
Raddost <raddost@gmail.com>
2015-12-23 18:12:23 +0200

    23.12

--- Towel/Program.cs
+++ Towel/Program.cs
@@ -10,14 +10,36 @@ namespace Towel
     {
         static void Main(string[] args)
         {
-            int width = int.Parse(Console.ReadLine());
-            int height = (int) Math.Floor(width * 1.5);
-            string[] output = ;
+            /* int width = int.Parse(Console.ReadLine());
+             int height = (int) Math.Floor(width * 1.5);
+             string[] output = ;
 
-            for (int i = 0; i < 51; i++)
+             for (int i = 0; i < 51; i++)
+             {
+                 output[i] = "#..";
+             }*/
+
+            int width = int.Parse(Console.ReadLine());
+            int height = (int)Math.Floor(width * 1.5);
+
+            int totalSymbolsCount = width * height;
+            for (int row = 0; row < height; row++)
             {
-                output[i] = "#..";
+                for (int col = 0; col < width; col++)
+                {
+                    if ((row + col) % 3 == 0)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write('.');
+                    }
+                }
+
+                Console.WriteLine();
             }
         }
     }
-}
+    }
+
